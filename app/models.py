51be57e219de48e12190f63fffa12bff006ee7ff
Dandi Maulana <dandimaulana1994@gmail.com>
2025-06-23 18:17:17 +0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,7 @@
 
+from app import db
+
+class Restaurant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    review = db.Column(db.String(500), nullable=True)
