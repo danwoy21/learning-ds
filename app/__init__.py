7337f809174b265ea1db1c04ab197aedc8fd58ac
Dandi Maulana <dandimaulana1994@gmail.com>
2025-06-23 18:16:50 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/restaurant_reviews'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+from app import routes
 
